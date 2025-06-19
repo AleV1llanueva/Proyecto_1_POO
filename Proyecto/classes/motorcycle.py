7d7967d5e38361ddd921a3ea632c93bfd674c6cb
AleV1llanueva <163602565+AleV1llanueva@users.noreply.github.com>
2025-06-19 11:57:53 -0600

    Add files via upload

---
+++ Proyecto/classes/motorcycle.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass, asdict
+from bson import ObjectId
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+
+@dataclass
+class Motorcycle:
+    brand:str
+    model:str
+    color:str
+    vin:str
+    owner:str
+    licensePlateId:str = "-"
+        
+    def hasA (self, id:str):
+        self.licensePlateId = ObjectId(id)
+        
+    def __str__(self):
+        return (
+            f"🔹 Brand : {self.brand}\n"
+            f"🔹 Model : {self.model}\n"
+            f"🔹 Color : {self.color}\n"
+            f"🔹 VIN : {self.vin}\n"
+            f"🔹 Owner : {self.owner}"
+        )   
+        
+    def save(self, coll):
+        return str(coll.insert_one(asdict(self)).inserted_id)
+    
+m =  Motorcycle("Yamaha", "YZ", "black", "JGF7G38F", "Federico")
+print(m)
